c6ce848413cbacd0011758ea1fec3c144a70b5ed
yu <email@yuyuyuyuyu.dev>
2025-07-19 14:21:14 +0900

    feat(androidexample): create sample app

--- androidexample/src/main/java/dev/yuyuyuyuyu/android/androidexample/MainActivity.kt
+++ androidexample/src/main/java/dev/yuyuyuyuyu/android/androidexample/MainActivity.kt
@@ -4,45 +4,47 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.TopAppBar
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import dev.yuyuyuyuyu.android.androidexample.ui.theme.MyMaterialThemeTheme
 import dev.yuyuyuyuyu.mymaterialtheme.MyMaterialTheme
 
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             MyMaterialTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        TopAppBar(title = { Text("MyMaterialTheme Example") })
+                    },
+                ) { innerPadding ->
+                    Column(
+                        modifier = Modifier.padding(innerPadding),
+                    ) {
+                        Row {
+                            Button(onClick = {}) {
+                                Text("Button")
+                            }
+
+                            OutlinedButton(onClick = {}) {
+                                Text("OutlinedButton")
+                            }
+                        }
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyMaterialThemeTheme {
-        Greeting("Android")
     }
 }
